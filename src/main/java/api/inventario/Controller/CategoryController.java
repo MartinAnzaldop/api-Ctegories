1d8f88c27632bbcbe9fff7df9ee9712055b6b462
MartinAnzaldop <126999311+MartinAnzaldop@users.noreply.github.com>
2023-04-06 11:52:35 -0600

    Update CategoryController.java

--- src/main/java/api/inventario/Controller/CategoryController.java
+++ src/main/java/api/inventario/Controller/CategoryController.java
@@ -1,22 +1,3 @@
-package api.inventario.Controller;
-
-import java.util.ArrayList;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import api.inventario.Model.Category;
-import api.inventario.Service.CategoryService;
-
 @RestController
 @RequestMapping("/inventary")
 @CrossOrigin(origins = "*")
@@ -30,19 +11,17 @@ public class CategoryController {
 		return this.categoryService.getCategory();
 	}
 	
-	@PostMapping
-	public Category saveCategory(@RequestBody Category category) {
-		return this.categoryService.saveCategory(category);
-	}
+	
 	
 	@GetMapping(path= "/{id}")
 	public Optional<Category> getCategoryById(@PathVariable("id") Long id){
 		return this.categoryService.getById(id);
 	}
 	
-	@PutMapping(path= "/{id}")
-	public Category updateCategoryById(@RequestBody Category requets, @PathVariable("id") Long id) {
-		return this.categoryService.updateById(requets, id);
+	@PostMapping
+	public ResponseEntity<Category> save(@RequestBody Category category){
+		Category obj=categoryService.saveCategory(category);
+		return new ResponseEntity<Category>(obj, HttpStatus.OK);
 	}
 	
 	@DeleteMapping(path= "/{id}")
